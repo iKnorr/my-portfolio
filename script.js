fdb54051325d3c90ca1e4197619ad50e206ecac7
Ingo Knorr <ingoknorr@gmail.com>
2022-06-05 12:04:15 +0200

    added animation to nacbar icons and lights on/off

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ const cardContact = document.querySelector('.card-contact');
 const btnOpenContactForm = document.querySelector('.open-contact-form');
 const btnCloseContactForm = document.querySelector('.close-contact-form');
 
+// Lights off/on
+const job = document.querySelector('.job');
+const nameHeader = document.querySelector('.name');
+const btnLightsOff = document.querySelector('.light-bulb');
+
+// Turn lights off
+btnLightsOff.addEventListener('click', function () {
+  job.classList.toggle('lights-on-off');
+  nameHeader.classList.toggle('lights-on-off');
+  btnLightsOff.classList.toggle('light-bulb-off');
+  console.log('hello world');
+});
+
 // Open contact form
 btnOpenContactForm.addEventListener('click', function () {
   cardContact.classList.remove('hidden');
